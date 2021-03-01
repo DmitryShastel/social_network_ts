e63a1097e33d5257aa17f9e8e0bdb4d0b18a0656
DmitryShastel <dmshastel@gmail.com>
2021-03-01 13:43:28 +0300

    social_network 1-52

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -13,15 +13,19 @@ export type UserType = {
 
 export let Users = (props: UserType) => {
 
-    if (props.users.length === 0) {
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
 
+        }
     }
 
+
     return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
             props.users.map( u => <div key={u.id}>
                 <span>
